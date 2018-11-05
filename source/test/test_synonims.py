02abc91e7939a6a191f6ab6b3695f7848d19120e
David Arroyo Menendez <davidam@gmail.com>
2018-11-05 16:30:31 +0100

    test: creation

---
+++ source/test/test_synonims.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import unittest
+from nltk.corpus import wordnet
+
+class TddInPythonExample(unittest.TestCase):
+
+    def test_synonims_definition_method_returns_correct_result(self):
+        syns = wordnet.synsets("program")
+        s = syns[0].definition()
+        self.assertEqual("a series of steps to be carried out or goals to be accomplished", s)
+
+    def test_synonims_lemmas_method_returns_correct_result(self):
+        syns = wordnet.synsets("program")
+        s = syns[0].lemmas()[0].name()
+        self.assertEqual("plan", s)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
