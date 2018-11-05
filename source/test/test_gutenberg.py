cc99f939f205f2c0236e8fd80f124b36cdec4883
David Arroyo Menendez <davidam@gmail.com>
2018-11-05 17:59:51 +0100

    test_gutenberg.py: creation

---
+++ source/test/test_gutenberg.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import unittest
+from nltk.book import *
+
+class TddInPythonExample(unittest.TestCase):
+
+    def test_concordance_method_returns_correct_result(self):
+        length = len(text3)
+        self.assertEqual(length, 44764)
