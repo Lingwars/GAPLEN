14ec089fd0c7d25900487157225e75dcd34a439d
David Arroyo Menendez <davidam@gmail.com>
2018-11-08 13:14:31 +0100

    corpusotherlanguages.py: fix parenthesis

--- source/corpusotherlanguages.py
+++ source/corpusotherlanguages.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.corpus import cess_esp
 from nltk.corpus import floresta
 
-print(nltk.corpus.cess_esp.words()
+print(nltk.corpus.cess_esp.words())
 print(nltk.corpus.floresta.words())
 print(nltk.corpus.indian.words('hindi.pos'))
 print(nltk.corpus.udhr.fileids())
